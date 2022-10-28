a01c1ec4c120dd580dfd7bf39681d8869c69ee2a
ac-png <alicecorry@icloud.com>
2022-10-28 10:25:18 +0100

    Fixing error messages on form

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -6,7 +6,6 @@ use App\Models\Note;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Str;
-use Mockery\Matcher\Not;
 
 class NoteController extends Controller
 {
